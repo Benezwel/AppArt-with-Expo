7efe6a2d0e7307a2d3675ec04fd2b033e63f0c1b
Benezwel <benezwwel@gmail.com>
2025-11-13 15:52:31 +0100

    mise en retrait des fonts. je n'en n'ai pas installer docoup y en a pas

--- app/(tab)/_layout.tsx
+++ app/(tab)/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+//   // Selection de la font dont on a besoin 
+// const [loaded] = useFonts({
+//   SpaceMono: require('assets/fonts/SpaceMono-Regular.ttf')
+// });
+
+// // condition pour utiliser l'effet SplashScree que l'on a importer de expo font
+// useEffect(() => {
+//   if (loaded){
+//     SplashScreen.hide();
+//   }
+// }, [loaded]);
+// if (!loaded){
+//   return null;
+// }
   return <Stack />;
 }
