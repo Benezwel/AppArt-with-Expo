2e7a52920fa09d805dca34da07da379bedc2217b
Benezwel <benezwwel@gmail.com>
2025-11-14 14:30:50 +0100

    added Tab in entry point layout , cretion of a tab need to understand more

--- app/(tab)/_layout.tsx
+++ app/(tab)/_layout.tsx
@@ -1,23 +1,18 @@
-import { Stack } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Tabs } from "expo-router";
 
 
 
 
-export default function RootLayout() {
-
-//   // Selection de la font dont on a besoin 
-// const [loaded] = useFonts({
-//   SpaceMono: require('assets/fonts/SpaceMono-Regular.ttf')
-// });
-
-// // condition pour utiliser l'effet SplashScree que l'on a importer de expo font
-// useEffect(() => {
-//   if (loaded){
-//     SplashScreen.hide();
-//   }
-// }, [loaded]);
-// if (!loaded){
-//   return null;
-// }
-  return <Stack />;
+export default function TabLayout() {
+  return <Tabs> 
+    <Tabs.Screen
+     name="index" 
+     options={{
+      title: 'Home',
+      tabBarIcon: ({color}) => < MaterialIcons size={28} />,
+     
+    }}
+     />
+  </Tabs>
 }
