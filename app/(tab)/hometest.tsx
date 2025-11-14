b7c2c1a07fdc48e76a5f3849723efb7a65e92c2d
Benezwel <benezwwel@gmail.com>
2025-11-14 12:58:58 +0100

    added import view and text in home test

---
+++ app/(tab)/hometest.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function viewTest (){
+    <View
+        style={{
+            alignItems: "center",
+
+
+        }}
+        >
+        <Text> 
+            Marcel Duchamp
+        </Text>
+    </View>
+}
