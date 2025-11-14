581fea54de16d188201dd58df630a1e7976addce
Benezwel <benezwwel@gmail.com>
2025-11-14 13:34:54 +0100

    added StackScreen in stack of layout home with the name of the testing route hometest

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 
 export default function StackLayout() {
-  return <Stack />;
+  return (
+    <Stack>  
+      <Stack.Screen name="hometest" options={{headerShown: false}} />
+       </Stack>
+  )
 }
