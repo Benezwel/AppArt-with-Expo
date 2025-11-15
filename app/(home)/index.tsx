ad21dd8d6643c0b3bd52660e5400965cad5cf2a0
Benezwel <benezwwel@gmail.com>
2025-11-15 22:40:05 +0100

    small space in the index with the button

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,7 +1,9 @@
-import { Link } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { Button, Pressable, Text, View } from "react-native";
 
 export default function Index() {
+  
+  const router = useRouter();
   return (
     <View
       style={{
@@ -14,9 +16,9 @@ export default function Index() {
       <Pressable> 
          <Text>Artist</Text> 
       </Pressable>
-     
-      
-      </Link>
+     </Link>
+
+     <Button title="DaDaisme" onPress={() =>  router.navigate('/[Apprentissage]/Artist')}/>
       <Text>
         <h1> HOME </h1>
 
