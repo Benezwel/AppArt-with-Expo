3bcd53111db13711e74d7ae5639a25d8535e84c7
Benezwel <benezwwel@gmail.com>
2025-11-22 18:05:28 +0100

    pb affiche de la stack??? pourquoi, may be i need to create a Tab folder, & redirect entryPoint layout to the tab folder with screen false

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,6 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
-
 export default function TabLayout() {
   return (
     
@@ -14,7 +14,12 @@ export default function TabLayout() {
       options={{
         tabBarIcon: ({color}) => <FontAwesome size={28} name='home' color={color} />
         }} />
-        <Tabs.Screen name="[Apprentissage]" options={{ title: "Learning"}} />
+        <Tabs.Screen name="[Apprentissage]" 
+        options={{ 
+          tabBarIcon: ({ color, focused}) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+          )
+        }} />
 
         <Tabs.Screen name="[Daily]" options={{ title: "Daily"}} />
         
