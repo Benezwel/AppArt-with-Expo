6bc4e825bfd6313b6addbb955bd7f75bfe19c021
Benezwel <benezwwel@gmail.com>
2025-11-26 14:48:13 +0100

    first try seeing my tabs

---
+++ app/(tabs)/(Learning)/Mouvement/index.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from "expo-router";
+import { Text, View } from "react-native";
+
+
+export default function Index() {
+  const router = useRouter();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    
+  
+      {/* <Button 
+        title=" return back"
+        onPress={() => router.push('./[Daily]/second')} /> */}
+
+      <Text>
+        <h1>Artiste</h1>
+
+        </Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    
+    <Tabs
+    // screenOptions={ {
+    // tabBarActiveBackgroundColor: 'blue',}}
+    >  
+
+
+      <Tabs.Screen name="(Learning)" 
+      options={{
+        tabBarIcon: ({color}) => <FontAwesome size={28} name='home' color={color} />
+        }} />
+        <Tabs.Screen name="Discovery" 
+        options={{ 
+          tabBarIcon: ({ color, focused}) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+          )
+        }} />
+
+        <Tabs.Screen name="Daily" options={{ title: "Daily"}} />
+        
+        <Tabs.Screen name="Profile" options={{ title: "Profile"}} />
+        
+        
+       </Tabs>  
+       
+  )
+}
