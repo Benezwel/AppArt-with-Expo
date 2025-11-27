6a1a966d331c6e6c42946800d5ea9f862ef52825
Benezwel <benezwwel@gmail.com>
2025-11-27 14:22:01 +0100

    start modifiying index of Learning

--- app/(tabs)/(Learning)/index.tsx
+++ app/(tabs)/(Learning)/index.tsx
@@ -1,34 +1,26 @@
-import { H1 } from "@expo/html-elements";
-import { Image } from "expo-image";
 import { Link, useRouter } from "expo-router";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function Index() {
   
   const router = useRouter(); 
-  const DuchampImage = require('@/assets/images/lol.avif')
   return (
     <View
       style= {styles.container}>
 
-      <Image 
-      style={styles.Image}
-      source={DuchampImage}/>
 
      <Button title="DaDaisme"  onPress={() =>  router.navigate('/Mouvement')}/>
    
     
     {/* Essayer de mettre plusieur Link dans la default Route */}
 
-   <Button title="Daily" onPress={() => router.navigate("./[Daily]/second")} />
-    <Text style={styles.text}>
-       <H1>Home</H1> 
-    </Text>
+  
     
     <Link href={{
       pathname: "./[Profile]/third",
       params:  { Profile : 'profile'}
     }}> 
+    
     <Button title="Profile"/>
     </Link>
     
@@ -47,8 +39,8 @@ export default function Index() {
   },
   Image: {
     flex : 1,
-    width: '100%',
-    height: '100%'
+    width: '50%',
+    height: '50%'
   }
  })
 
