157dff6a611522983f5acbfa6031aad8c7613c40
Benezwel <benezwwel@gmail.com>
2025-11-27 10:31:33 +0100

    deleted Tabs of Learning route Layout

--- app/(tabs)/(Learning)/_layout.tsx
+++ app/(tabs)/(Learning)/_layout.tsx
@@ -1,33 +1,9 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 
-export default function TabLayout() {
+export default function StackLearning() {
   return (
     
-    <Tabs
-    // screenOptions={ {
-    // tabBarActiveBackgroundColor: 'blue',}}
-    >  
+    <Stack/> 
 
-
-      <Tabs.Screen name="index" 
-      options={{
-        tabBarIcon: ({color}) => <FontAwesome size={28} name='home' color={color} />
-        }} />
-        <Tabs.Screen name="[Apprentissage]" 
-        options={{ 
-          tabBarIcon: ({ color, focused}) => (
-            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
-          )
-        }} />
-
-        <Tabs.Screen name="Daily" options={{ title: "Daily"}} />
-        
-        <Tabs.Screen name="Profile" options={{ title: "Profile"}} />
-        
-        
-       </Tabs>  
-       
   )
 }
