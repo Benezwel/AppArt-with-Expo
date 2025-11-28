47e1684aea30fb7477fc255653b121adbaddc410
Benezwel <benezwwel@gmail.com>
2025-11-28 11:25:17 +0100

    added  folder for introduction in Learning, not a tab only a stack.

---
+++ app/(tabs)/(Learning)/1Mouvement/Introduction/index.tsx
@@ -0,0 +1,28 @@
+import { H1 } from "@expo/html-elements";
+import { useRouter } from "expo-router";
+import { Text, View } from "react-native";
+
+
+export default function Index() {
+  const router = useRouter();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    
+  
+      {/* <Button 
+        title=" return back"
+        onPress={() => router.push('./[Daily]/second')} /> */}
+
+      <Text>
+        <H1>Mouvement</H1>
+
+        </Text>
+    </View>
+  );
+}
