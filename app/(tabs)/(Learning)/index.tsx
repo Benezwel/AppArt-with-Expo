96b1e93206c52789455b6c910e74b2730f18badf
Benezwel <benezwwel@gmail.com>
2026-01-09 11:24:27 +0100

    little change

--- app/(tabs)/(Learning)/index.tsx
+++ app/(tabs)/(Learning)/index.tsx
@@ -21,11 +21,11 @@ export default function Index() {
     </Text>
     </View>
 
-    <View style={{flex: 1, backgroundColor: 'red'}} />
+    {/* <View style={{flex: 1, backgroundColor: 'red'}} />
 
     <View style={{flex: 2, backgroundColor: '#8B1E1E'}} />
 
-    <View style={{flex: 3, backgroundColor: 'blue'}} />
+    <View style={{flex: 3, backgroundColor: 'blue'}} /> */}
 
     {/* Essayer de mettre plusieur Link dans la default Route */}
 
