dfe0179dcd55278a64ec5ae9585c040198da5c5e
Benezwel <benezwwel@gmail.com>
2026-01-20 16:34:17 +0100

    test nested componant

--- app/(tabs)/(Learning)/index.tsx
+++ app/(tabs)/(Learning)/index.tsx
@@ -7,26 +7,34 @@ import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native"
 
 
 export default function Index() {
-  
+  // on peut creer ses propre  compoant 
+  function Test(){
+    return(
+      <View>
+        <Text> Test pour montre le componant Test</Text>
+      </View>
+    )
+  };
   const router = useRouter(); 
   const PlaceHolder = require("@/assets/images/lol.avif");
+  const name = "ArtVanGarde"
+  
+  
   return (
      <ScrollView>
-    <View style= {{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "flex-start"
-    }}> 
-      
-     {/* <Button title="DaDaisme"  onPress={() =>  router.navigate('/1Mouvement')}/> */}
+    <View>
+      <Text>{name}</Text>
    <Image source={{
-    uri:"https://i.pinimg.com/736x/03/7b/31/037b3189716898809199b789208ee922.jpg"}}  />
     
-    <View style={styles.ImageContainer}> 
+  uri:"https://i.pinimg.com/736x/03/7b/31/037b3189716898809199b789208ee922.jpg"}}
+    style={{width: 500, height: 500}}  />
+
+   
     <Text>   
+      <Test/>
     <H1> Artistic Mouvement </H1>
     </Text>
-    </View>
+   
     <Text style={{
       alignItems: "center", 
     }}> Try to learn</Text>
